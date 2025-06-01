c23e63fe20d882c289e6a04f78d5f58a50dfc269
Claudio Barrios <cbmne.93@gmail.com>
2025-06-01 16:54:29 -0300

    user-model mongo database

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
 import { Server } from "./presentation/server";
 import { envs } from "./config/envs";
 import { AppRoutes } from "./presentation/routes";
+import { MongoDatabase } from "./data";
 
 (async () => {
   main();
 })();
 
-function main() {
+async function main() {
+
+  await MongoDatabase.connect({
+    dbName: envs.MONGO_DB_NAME,
+    mongoUrl:  envs.MONGO_URL,
+  })
+
+
   const server = new Server({
     port: envs.PORT,
     routes: AppRoutes.routes,
